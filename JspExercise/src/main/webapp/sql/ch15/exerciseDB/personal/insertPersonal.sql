8f24a5a3b3dbd6071a23fff8c04f6c325d1ca4b5
India_Ink <indiaink10@gmail.com>
2022-03-14 11:37:32 +0900

    Add chapter 15's sql scripts

---
+++ JspExercise/src/main/webapp/sql/ch15/exerciseDB/personal/insertPersonal.sql
@@ -0,0 +1,6 @@
+insert into personal values('park', '1234', '강원대', '010', '5678', 'park@naver.com', '010-1234-7643', '23456', '강원도 동해시 중앙로 346 강원대학교 삼척캠퍼스', '대학생');
+insert into personal values('kim', '4321', '삼척대', '011', '8765', 'kim@kangwon.com', '011-4321-2345', '12345', '강원도 삼척시 중앙로 346 강원대학교 삼척캠퍼스', '고등학생');
+insert into personal values('lee', 'lee123', '춘천대', '017', '5678', 'lee12@samcheok.com', '017-1234-3211', '25913', '강원도 춘천시 중앙로 346 강원대학교 삼척캠퍼스', '취업생');
+insert into personal values('kang', 'knag1234', '동해대', '016', '2345', 'kang32@naver.com', '016-1234-5678', '44567', '강원도 강릉시 중앙로 346 강원대학교 삼척캠퍼스', '직장인');
+
+select * from personal;
---
+++ JspExercise/src/main/webapp/sql/ch15/exerciseDB/student/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO STUDENT (num, pwd, depart, name, address, phone, email) VALUES (2018100001, '12', '모바일과', '홍길순', '서울시','010-9002-1234','ab@naver.com');
+INSERT INTO STUDENT (num, pwd, depart, name, address, phone, email) VALUES (2018100002, 'qw', '모바일과', '홍길동', '경기도','010-2009-4321','qwe@kangwon.ac.kr');
+INSERT INTO STUDENT (num, pwd, depart, name, address, phone, email) VALUES (2018200001, 'qwe', '영어과', '수여인', '인천시','010-3918-0007','zxc@a.c');
+INSERT INTO STUDENT (num, pwd, depart, name, address, phone, email) VALUES (2018200002, '34', '영어과', '김다운', '서울시','010-3002-0101','aqz@daum.net');
+
+select * from student;
